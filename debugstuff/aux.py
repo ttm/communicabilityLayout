7941cdd2aa1747256486bbbdd83a1ac00bc709c7
Renato Fabbri <renato.fabbri@gmail.com>
2019-07-13 12:41:33 -0300

    terminado perfil geral do processamento

--- debugstuff/aux.py
+++ debugstuff/aux.py
@@ -102,3 +102,62 @@ def cmdscale(D):
     Y  = V.dot(L)
 
     return Y, evals
+
+import sympy
+import numpy as np
+def give_coords(distances):
+    """give coordinates of points for which distances given
+
+    coordinates are given relatively. 1st point on origin, 2nd on x-axis, 3rd 
+    x-y plane and so on. Maximum n-1 dimentions for which n is the number
+    of points
+
+     Args:
+        distanes (list): is a n x n, 2d array where distances[i][j] gives the distance 
+            from i to j assumed distances[i][j] == distances[j][i]
+
+     Returns:
+        numpy.ndarray: cordinates in list form n dim
+
+     Examples:
+        >>> a = sympy.sqrt(2)
+        >>> distances = [[0,1,1,1,1,1],
+                         [1,0,a,a,a,a],
+                         [1,a,0,a,a,a],
+                         [1,a,a,0,a,a],
+                         [1,a,a,a,0,a],
+                         [1,a,a,a,a,0]]
+        >>> give_coords(distances)
+        array([[0, 0, 0, 0, 0],
+               [1, 0, 0, 0, 0],
+               [0, 1, 0, 0, 0],
+               [0, 0, 1, 0, 0],
+               [0, 0, 0, 1, 0],
+               [0, 0, 0, 0, 1]], dtype=object)
+
+        >>> give_coords([[0, 3, 4], [3, 0, 5], [4, 5, 0]])
+        array([[0, 0],
+        [3, 0],
+        [0, 4]], dtype=object)        
+
+    """
+    distances = np.array(distances)
+
+    n = len(distances)
+    X = sympy.symarray('x', (n, n - 1))
+
+    for row in range(n):
+        X[row, row:] = [0] * (n - 1 - row)
+
+    for point2 in range(1, n):
+
+        expressions = []
+
+        for point1 in range(point2):
+            expression = np.sum((X[point1] - X[point2]) ** 2) 
+            expression -= distances[point1,point2] ** 2
+            expressions.append(expression)
+
+        X[point2,:point2] = sympy.solve(expressions, list(X[point2,:point2]))[1]
+
+    return X
--- debugstuff/server11072019.py
+++ debugstuff/server11072019.py
@@ -3,17 +3,50 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.linalg import expm
 # https://scikit-learn.org/stable/modules/generated/sklearn.manifold.smacof.html
-# from sklearn.manifold import MDS
-from sklearn.manifold import smacof
+from sklearn.manifold import MDS, Isomap, LocallyLinearEmbedding
 # http://lvdmaaten.github.io/publications/papers/JMLR_2014.pdf
 from MulticoreTSNE import MulticoreTSNE as TSNE
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, SpectralClustering, AffinityPropagation
 from sklearn.metrics import silhouette_score
 from sklearn.decomposition import PCA
 import umap  # https://umap-learn.readthedocs.io/en/latest/
 # try megaman
 
 from aux import *
+def decompose(dimred, dim):
+    if dimred == 'MDS': # slowest!
+        embedding = MDS(n_components=dim, n_init=__inits, max_iter=__iters, n_jobs=-1, dissimilarity=__dis)
+    elif dimred == 'ISOMAP': # slow
+        embedding = Isomap(n_neighbors=__nneigh, n_components=dim, n_jobs=-1)
+    elif dimred == 'LLE': # slow-acceptable
+        embedding = LocallyLinearEmbedding(n_neighbors=__nneigh, n_components=dim, n_jobs=-1)
+    elif dimred == 'TSNE': # acceptable
+        embedding = TSNE(n_components=dim, n_iter=__iters, metric='precomputed', learning_rate=__lrate, perplexity=__perplexity)
+    elif dimred == 'UMAP': # fast
+        embedding = umap.UMAP(n_neighbors=__nneigh, n_components=dim, metric=__dis, min_dist=0.1)
+    elif dimred == 'PCA': # fastest!
+        embedding = PCA(n_components=dim)
+    else:
+        raise ValueError('dimension reduction method not recognized')
+
+    positions = embedding.fit_transform(An)
+    return positions
+
+def clust(clu, i):
+    if clu == 'KM':
+        calg = KMeans(n_clusters=i, n_init=100,n_jobs=-1)
+    elif clu == 'AG':
+        calg = AgglomerativeClustering(n_clusters=i)
+    elif clu == 'SP':
+        # calg = SpectralClustering(n_clusters=i, affinity='precomputed', n_jobs=-1)
+        calg = SpectralClustering(n_clusters=i, n_jobs=-1)
+    elif clu == 'AF':
+        # calg = AffinityPropagation(n_clusters=i, affinity='precomputed', n_jobs=-1)
+        calg = AffinityPropagation()
+    else:
+        raise ValueError('clustering algorithm not recognized')
+    res = calg.fit(pC)
+    return [int(j) for j in res.labels_]
 
 ##################### settings ###################
 ### communicability calculations:
@@ -25,24 +58,24 @@ __mangle = 10e-5
 __temp = 1
 
 ### dimensionality reduction:
-__dimred = 'ISOMAP'
-__dimred = 'ICA'
-# __dimred = 't-SNE'
 __dis = 'euclidean'
 __dis = 'precomputed'
-__dimred = 'MDS'
-__dimred = 'PCA'  # implement, maybe also LDA or other methods
+# __dimred = 't-SNE'
 __dimred = 'UMAP'
-
-__dimredC = 'ISOMAP'
-__dimredC = 'ICA'
-__dimredC = 't-SNE'
-__dimredC = 'MDS'
-__dimredC = 'PCA'  # implement, maybe also LDA or other methods
-__dimredC = 'UMAP'
-__dimC = 3
-
+__dimred = 'ISOMAP'
+__dimred = 'MDS'
+__dimred = 'TSNE'
+__dimred = 'PCA'  # implement, maybe also LDA or other methods
 __dim = 3
+
+__dimredC = 'TSNE'
+__dimredC = 'MDS'
+__dimredC = 'UMAP'
+__dimredC = 'ISOMAP'
+__dimredC = 'TSNE'
+__dimredC = 'PCA'  # implement, maybe also LDA or other methods
+__dimC = 5
+
 __inits = 3  # for MDS ~3
 __inits = 1  # for MDS ~3
 __inits = 30  # for MDS ~3
@@ -50,7 +83,7 @@ __iters = 1000  # for MDS (~100) and t-SNE (~250)
 __iters = 1000  # for MDS (~100) and t-SNE (~250)
 __perplexity = 5  # for t-SNE
 __lrate = 12  # for t-SNE
-__nneigh = 3000  # for UMAP
+__nneigh = 300  # for UMAP
 
 __cdmethod = 'an'  # angles
 # __cdmethod = 'dist'  # remove this option dist
@@ -61,6 +94,10 @@ __nclu = 6  # maximum number of clusters, not used for now
 __cddim = 'nd'  # the dimmensionality in which community detection is performed
 __cddim = 'rd'  # the dimmensionality in which community detection is performed
 __cddim = 3  # the dimmensionality in which community detection is performed
+__clu = 'KM'
+__clu = 'AG'
+__clu = 'SP'
+__clu = 'AF'
 
 ### plotting
 plot = True
@@ -96,21 +133,7 @@ tt = t.time()
 
 # E_original = n.linalg.eigvals(An)
 
-if __dimred == 'MDS':
-    # embedding = MDS(n_components=__dim, n_init=__inits, max_iter=__iters, n_jobs=-1, dissimilarity=__dis)
-    embedding = smacof(An)
-    # foo = cmdscale(An)
-    # p = foo[0][:,:3]
-elif __dimred == 'PCA':
-    embedding = PCA(n_components= __dim)
-    p = positions = embedding.fit_transform(An)
-elif __dimred == 'UMAP':
-    embedding = umap.UMAP(n_neighbors=__nneigh, n_components=__dim, metric=__dis, min_dist=0)
-    p = positions = embedding.fit_transform(An)
-else:
-    embedding = TSNE(n_components=__dim, n_iter=__iters, metric='precomputed', learning_rate=__lrate, perplexity=__perplexity)
-    p = positions = embedding.fit_transform(An)
-# p = positions = embedding.fit_transform(X)
+p = decompose(__dimred, __dim)
 print('embedding', t.time() - tt)
 tt = t.time()
 
@@ -124,6 +147,15 @@ print('sphere', t.time() - tt)
 tt = t.time()
 
 # detecting communities
+if __cddim == N:
+    pC = An
+elif (__dimredC == __dimred) and (__dimC == __dim):
+    pC = p
+else:
+    pC = decompose(__dimredC, __dimC)
+    print('second embedding', t.time() - tt)
+    tt = t.time()
+
 km = []
 ev = []
 if __minclu == 1:
@@ -132,25 +164,21 @@ if __minclu == 1:
     km.append([0]*N)
 nclusts = list(range(__minclu, __nclu +1 ))
 
-if __cddim == N:
-    pC = An
-elif (__dimredC == __dimred) and (__dimC == __dim):
-    pC = p
+
+# Hierarchical clustering Ward
+# DBSCAN
+# Spectral Clustering
+if __clu in ('AF', 'DB'):
+    labels = clust(__clu, 0)
+    km.append(labels)
+    ev.append(1)
 else:
-    pC = dimRed(An, __dimC, __dimredC)
-
-
-for i in nclusts:
-    # kmeans = KMeans(n_clusters=i, n_init=100, max_iter=3000, n_jobs=-1, tol=1e-6).fit(p)
-    if __cddim == N:
-        kmeans = KMeans(n_clusters=i, n_init=100, max_iter=3000, n_jobs=-1, tol=1e-6).fit(An)
-    elif __cddim == __dim:
-        kmeans = KMeans(n_clusters=i, n_init=100, max_iter=3000, n_jobs=-1, tol=1e-6).fit(p)
-    else:
-        kmeans = KMeans(n_clusters=i, n_init=100, max_iter=3000, n_jobs=-1, tol=1e-6).fit(X_)
-    km.append([int(j) for j in kmeans.labels_])
-    score = silhouette_score(p, kmeans.labels_)
-    ev.append(score)
+    for i in nclusts:
+        # kmeans = KMeans(n_clusters=i, n_init=100, max_iter=3000, n_jobs=-1, tol=1e-6).fit(p)
+        labels = clust(__clu, i)
+        km.append(labels)
+        score = silhouette_score(pC, labels)
+        ev.append(score)
 print('clustering', t.time() - tt)
 tt = t.time()
 
@@ -168,23 +196,3 @@ if plot:
     ll = n.vstack( A.nonzero() ).T  # links
     ll_ = ll.reshape(ll.shape[0]*ll.shape[1])
     plot3d(p, ll_, km_)
-
-# if plot:
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111, projection='3d')
-# 
-#     print('plot kmeans')
-#     for c in range(max(km_) + 1):
-#         yes = n.array(km_) == c
-#         pp = p[yes]
-#         ax.scatter(pp[:,0], pp[:,1], pp[:,2])
-# 
-#     ll = n.vstack( A.nonzero() ).T.tolist()  # links
-#     for l in ll: 
-#         p0 = p[l[0]]
-#         p1 = p[l[1]]
-#         lp = n.vstack(( p0, p1 ))
-#         ax.plot(lp[:, 0], lp[:, 1], lp[:, 2], 'c')
-#     print('plot set', t.time() - tt)
-#     tt = t.time()
-#     plt.show()
